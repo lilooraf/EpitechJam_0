6b98c8cb40542afc74eca6cac5595eb262146565
Anthony Dijoux <anthonydijoux@pop-os.localdomain>
2021-01-30 20:36:44 +0400

    menu

---
+++ New Unity Project/Assets/MusicPlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour
+{
+
+    public AudioSource AudioSource;
+
+    private float musicVolume = 0.5f;
+
+    void Start()
+    {
+        AudioSource.Play();
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
